2db9060390469c5a9dd6cf0e53e24aacfb1f8346
Kamal-Capstone <m.kamal@zekab.com>
2024-01-26 10:51:09 +0500

    This is my first commit

---
+++ app/src/main/java/com/example/testingproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.testingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener() {
+            Toast.makeText(applicationContext, "Hey! how are you?", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
